8e4c97d7b4fa2deba393c1804f3342de0f96b66f
hikarangerthings <hikarangerthings@users.noreply.github.com>
2026-07-19 13:57:49 +0900

    Enable Cloudflare Web Analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: process.env.SITE_URL ?? 'https://hito-to-ba-no-aida.pages.dev',
+  site: process.env.SITE_URL ?? 'https://hito-to-ba-no-aida.hikaru-taniguchi-2023.workers.dev',
   integrations: [sitemap()],
   trailingSlash: 'always',
 });
